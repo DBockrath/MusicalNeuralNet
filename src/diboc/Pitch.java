a29ca2c8aa4e0b9af09ef1806245b731ebc12730
Dan Bockrath <dbockrath26@fairport.org>
2019-05-16 20:06:03 -0400

    Changed this to a class. Made a new enum called Pitches.

--- src/diboc/Pitch.java
+++ src/diboc/Pitch.java
@@ -1,7 +1,58 @@
 package diboc;
 
-public enum Pitch {
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
 
-    C, Db, D, Eb, E, F, Gb, G, Ab, A, Bb, B
+public class Pitch {
+
+    private Clip clip;
+    private AudioInputStream audioInputStream;
+
+    public Pitch(Clip clip, AudioInputStream audioInputStream) {
+
+        this.clip = clip;
+        this.audioInputStream = audioInputStream;
+
+    }
+
+    public Pitch(Pitches pitches) {
+
+        try {
+
+            clip = (Clip) AudioSystem.getLine(new DataLine.Info(Clip.class, AudioSystem.getAudioInputStream(new File("\\src\\diboc\\PianoPitches\\Piano.ff." + pitches.name() + ".aiff")).getFormat()));
+            audioInputStream = AudioSystem.getAudioInputStream(new File("\\src\\diboc\\PianoPitches\\Piano.ff." + pitches.name() + ".aiff"));
+
+        } catch (UnsupportedAudioFileException | LineUnavailableException | IOException e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+    public void play() {
+
+        try {
+
+            clip.open(audioInputStream);
+
+        } catch (LineUnavailableException | IOException e) {
+
+            e.printStackTrace();
+
+        }
+
+        clip.setFramePosition(0);
+        clip.start();
+
+    }
+
+    public void stop() {
+
+        clip.stop();
+        clip.close();
+
+    }
 
 }
